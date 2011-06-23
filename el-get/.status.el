8d13017959ac6ddf07915c7038ff4617233f7ec3
Pat Maddox <patmaddox@me.com>
2011-06-23 14:29:10 -0700

    get rid of stupid elpa and el-get

--- el-get/.status.el
+++
@@ -1,1 +0,0 @@
-(:ruby-mode "installed" :inf-ruby "installed" :ruby-compilation "installed" :css-mode "installed" :textmate "installed" :rvm "installed" :rhtml "installed" :yaml-mode "installed" :cheat "installed" :gist "installed" :magit "installed" :feature-mode "installed" :yasnippet-bundle "installed")
